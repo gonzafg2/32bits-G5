15fc2faf35c9d251225caef703ded03bfd9c8f96
Gonzalo Fleming <gonzafg2@gmail.com>
2021-04-27 22:15:53 -0400

    Carga inicial

---
+++ src/views/Busquedas.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1 class="text-center my-5">Listado de Juegos con stock</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-12" v-if="juegos">
+          <TablaJuegos :productos="juegos" />
+        </div>
+        <div class="col-12" v-else>
+          <h2>No es posible traer los juegos</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TablaJuegos from "@/components/TablaJuegos.vue";
+import { mapState } from "vuex";
+
+export default {
+  name: "Busquedas",
+  components: {
+    TablaJuegos,
+  },
+  computed: {
+    ...mapState(["juegos"]),
+  },
+};
+</script>
